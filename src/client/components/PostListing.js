3d9f2ee284f251fff3618db5a4bc987135589c0b
Lillian Lee <lillianlee118@gmail.com>
2017-10-07 18:28:48 -0700

    contact sends email

--- src/client/components/PostListing.js
+++ src/client/components/PostListing.js
@@ -5,15 +5,14 @@ import submitListing from '../utils/submitListing.js';
 import jwt from 'jsonwebtoken';
 
 export default class PostListing extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       name: '',
+      email: '',
       zipcode: '',
       dogSizePreference: '',
       dogBreedPreference: '',
-      dogTemperamentPreference: '',
       dogActivityPreference: '',
       homeAttributes: '',
       hostPictures: null,
@@ -40,40 +39,32 @@ export default class PostListing extends React.Component {
           return;
         }
       }
-
       let formData = new FormData();
       formData.append("name", this.state.name);
+      formData.append("email", this.state.email);
       formData.append("zipcode", this.state.zipcode);
       formData.append("dogSizePreference", this.state.dogSizePreference);
       formData.append("dogBreedPreference", this.state.dogBreedPreference);
-      formData.append("dogTemperamentPreference", this.state.dogTemperamentPreference);
+      // formData.append("dogTemperamentPreference", this.state.dogTemperamentPreference);
       formData.append("dogActivityPreference", this.state.dogActivityPreference);
       formData.append("homeAttributes", this.state.homeAttributes);
       formData.append("hostPictures", this.state.hostPictures);
       formData.append("homePictures", this.state.homePictures);
       formData.append("cost", this.state.cost);
-
-
-
       for (var pair of formData.entries()) {
         console.log(pair[0] + ', ' + pair[1]);
       }
-
       let url = 'http://localhost:3000/listings';
-
       submitListing(url, formData, (res) => {
         if (res.success === true) {
           console.log('Listing submitted!');
           this.setState({message: res.message});
           this.setState({submitted: true});
-
         } else {
           console.log('Error: ', res.error);
         }
       });
-
     }
-
   }
 
   componentDidMount() {
@@ -81,7 +72,6 @@ export default class PostListing extends React.Component {
     let decoded = jwt.decode(token);
     console.log(decoded);
     this.setState({name: decoded.name});
-
   }
 
   render() {
@@ -92,7 +82,6 @@ export default class PostListing extends React.Component {
     } else {
       return (
         <div>
-
           <h1>Sign up to become a pet host:</h1>
           {this.state.error ? <p className="postListing-error">{this.state.error}</p> : ''}
           <form onChange={this.setField} onSubmit={this.handleSubmit}>
@@ -100,37 +89,26 @@ export default class PostListing extends React.Component {
               <div className="postListing-label">
                 <label>Name:</label><br />
                 <input type="text" name="name" value={this.state.name} /><br />
-
+                <label>Email:</label><br />
+                <input type="text" name="email" value={this.state.email} /><br />
                 <label>Zip Code:</label><br />
                 <input type="number" name="zipcode" value={this.state.zipcode} /><br />
-
                 <label>Dog Size Preference</label><br />
                 <input type="text" name="dogSizePreference" value={this.state.dogSizePreference} /><br />
-
-                <label>Dog Temperament Preference:</label><br />
-                <input type="text" name="dogTemperamentPreference" value={this.state.dogTemperamentPreference} /><br />
-
                 <label>Dog Breed Preference:</label><br />
                 <input type="text" name="dogBreedPreference" value={this.state.dogBreedPreference} /><br />
               </div>
               <div className="postListing-label">
                 <label>Dog Activity Preferences:</label><br />
                 <input type="text" name="dogActivityPreference" value={this.state.dogActivityPreference} /><br />
-
                 <label>Cost Per Night: </label><br />
                 <input type="text" name="cost" value={this.state.cost} /><br />
-
                 <label>Description:</label><br />
                 <textarea type="text" name="homeAttributes" value={this.state.homeAttributes} /><br />
-
-
                 <label htmlFor="hostPictures" className="postListing-fileLabel">{this.state.hostPictures ? this.state.hostPictures.name : `Choose a Picture of you`}</label><br />
                 <input type="file" name="hostPictures" id="hostPictures" className="postListing-file" /><br />
-
                 <label htmlFor="homePictures" className="postListing-fileLabel">{this.state.homePictures ? this.state.homePictures.name : `Choose a Picture of your home`}</label><br />
                 <input type="file" name="homePictures" id="homePictures" className="postListing-file" /><br />
-
-
               </div>
             </div>
             <div >
@@ -144,7 +122,6 @@ export default class PostListing extends React.Component {
               />
             </div>
           </form>
-
         </div>
       );
     };
