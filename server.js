4ec34720b942debd72353da0938e30df9c7204e0
Lillian Lee <lillianlee118@gmail.com>
2017-10-07 18:34:09 -0700

    changes

--- server.js
+++ server.js
@@ -252,7 +252,7 @@ app.post('/contacthost', (req, res) => {
   var mailOptions = {
     to: hostEmail,
     subject: 'Hi! Someone wants to stay at your house!',
-    text: 'email the petowner @ ' + ownerEmail + 'before ' + date
+    text: 'email the petowner @ ' + ownerEmail + ' before ' + date
   };
   transporter.sendMail(mailOptions, function(error, response) {
     if (error) {
